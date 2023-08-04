4adfab534d082c2cb1d3c33513045b5a19534943
MirMoheen12 <mirmoheen9@gmail.com>
2023-08-04 23:29:36 +0500

    Add project files.

---
+++ Exepense_Vendor_Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exepense_Vendor_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
