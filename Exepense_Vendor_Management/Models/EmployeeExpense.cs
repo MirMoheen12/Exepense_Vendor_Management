1539969e540be4157c82d6b32d878cdb809ebfc3
kainat Malik <kainat Malik@DESKTOP-QGAODMM>
2023-08-08 15:17:33 -0700

    forms vendor,employee expense,costcenter expence are done

--- Exepense_Vendor_Management/Models/EmployeeExpense.cs
+++ Exepense_Vendor_Management/Models/EmployeeExpense.cs
@@ -12,14 +12,14 @@ namespace Exepense_Vendor_Management.Models
         public string createdBy { get; set; }
         public DateTime submissionDate { get; set; }    
         public float amount { get; set; }
-        public DateTime expenseAccurred { get; set; }
+        public DateTime expenseOccurred { get; set; }
         public string expenseCategory { get; set; }
 
         public string vandorName { get;set; }
 
         public string supportingDocs { get; set; }
 
-        public string status { get; set; }
+        public string status { get; set; } 
 
         public string notes { get; set; }
 
