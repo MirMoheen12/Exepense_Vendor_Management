5fd311618bb7174c01f7689b24cf52fc7e67c0cd
MirMoheen12 <mirmoheen9@gmail.com>
2023-08-26 16:28:25 +0500

    media

---
+++ Exepense_Vendor_Management/Controllers/AllMediaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exepense_Vendor_Management.Controllers
+{
+    [AllowAnonymous]
+    public class AllMediaController : Controller
+    {
+        public IActionResult MediaByid(int id,string reqtype)
+        {
+            return View();
+        }
+    }
+}
