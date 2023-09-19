3ef3e824a79bdc892d33d263ae7c9c282cd1a663
MirMoheen12 <mirmoheen9@gmail.com>
2023-09-19 12:42:14 +0500

    add members

---
+++ Exepense_Vendor_Management/Migrations/20230919070747_ksfdf.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Vendor_Management.Migrations
+{
+    public partial class ksfdf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactName",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhone",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactWebsite",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomerAccess",
+                table: "Vendor",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TechnolgyVendor",
+                table: "Vendor",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "ContactName",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPhone",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "ContactWebsite",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerAccess",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "TechnolgyVendor",
+                table: "Vendor");
+        }
+    }
+}
